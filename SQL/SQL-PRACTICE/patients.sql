2ce4a330d25bfa01a67a05d08f46b45c6d6d5f03
Mark <86335247+Mokoth@users.noreply.github.com>
2024-03-07 15:55:41 +0300

    Update patients.sql

--- SQL/SQL-PRACTICE/patients.sql
+++ SQL/SQL-PRACTICE/patients.sql
@@ -116,3 +116,13 @@ FROM patients
 GROUP BY city
 HAVING province_id = 'NS';
 
+-- Show unique birth years from patients and order them by ascending.
+SELECT
+  DISTINCT YEAR(birth_date) AS birth_year
+FROM patients
+ORDER BY birth_year;
+
+-- OR
+SELECT year(birth_date)
+FROM patients
+GROUP BY year(birth_date)
