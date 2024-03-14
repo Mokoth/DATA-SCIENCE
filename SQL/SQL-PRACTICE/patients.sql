0e66d7fde13707595ebce4d5410e6041ab8de66c
Mark <86335247+Mokoth@users.noreply.github.com>
2024-03-14 16:50:51 +0300

    Update patients.sql

--- SQL/SQL-PRACTICE/patients.sql
+++ SQL/SQL-PRACTICE/patients.sql
@@ -126,3 +126,47 @@ ORDER BY birth_year;
 SELECT year(birth_date)
 FROM patients
 GROUP BY year(birth_date)
+
+-- Show unique first names from the patients table which only occurs once in the list.
+-- For example, if two or more people are named 'John' in the first_name column then don't include their name in the output list. If only 1 person is named 'Leo' then include them in the output.
+SELECT first_name
+FROM patients
+GROUP BY first_name
+HAVING COUNT(first_name) = 1
+
+-- OR
+  
+SELECT first_name
+FROM (
+    SELECT
+      first_name,
+      count(first_name) AS occurrencies
+    FROM patients
+    GROUP BY first_name
+  )
+WHERE occurrencies = 1
+
+-- Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long.
+SELECT patient_id, 
+  first_name 
+FROM patients
+WHERE first_name LIKE 's%s' AND	length(first_name) >= 6;
+
+-- OR
+SELECT
+  patient_id,
+  first_name
+FROM patients
+WHERE first_name LIKE 's____%s';
+
+-- OR
+SELECT
+  patient_id,
+  first_name
+FROM patients
+where
+  first_name like 's%'
+  and first_name like '%s'
+  and len(first_name) >= 6;
+
+
